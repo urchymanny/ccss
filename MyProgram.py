e09378d98c0571011ba6f82fb88eeb94cb0ff10c
Mba Uchenna <urchymanny@gmail.com>
2023-10-31 15:22:00 +0000

    Update MyProgram.py

--- MyProgram.py
+++ MyProgram.py
@@ -1,1 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Oct 30 21:38:44 2023
 
+@author: nasir.ibrahim
+"""
+
+def add(first_number, second_number):
+    return first_number + second_number
+
+def subtract(first_number, second_number):
+    return first_number - second_number
+
+def multiply(first_number, second_number):
+    return first_number * second_number
+
+def divide(first_number, second_number):
+    return first_number / second_number
+    
